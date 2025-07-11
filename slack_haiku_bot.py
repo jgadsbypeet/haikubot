253e4a3ad0098f19e7fbe1ac03ca0e215fe54fb2
jgadsbypeet <jgadsbypeet@gmail.com>
2025-07-11 08:50:08 +0100

    Update OpenAI values

--- slack_haiku_bot.py
+++ slack_haiku_bot.py
@@ -1,9 +1,10 @@
-from flask import Flask, request, jsonify
-import openai
 import os
+from flask import Flask, request, jsonify
+from openai import OpenAI
 
 app = Flask(__name__)
-client = openai.OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 @app.route('/haiku', methods=['POST'])
 def haiku():
